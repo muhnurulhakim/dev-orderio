1c10dfbd39fa52c20e703c3371f4881df9e4fa45
degiam <degiam@ymail.com>
2023-02-27 07:46:57 +0700

    Add jekyll-minifier

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@
  * Designed by Degiam [https://degiam.github.io]
  * Copyright (c) 2023
  */
+function test(selector) {
+	document.querySelectorAll('.bg-white').forEach(item => {
+		console.log(item.innerText)
+	})
+}
+test('.bg-white')
